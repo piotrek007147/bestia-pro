e831de6562197b7b1df31cdda29b499077bd7648
piotrek007147 <piotrek7777777777@gmail.com>
2025-05-07 02:07:33 +0200

    Create kruk.sh

---
+++ BESTIA/KRUK/kruk.sh
@@ -0,0 +1,3 @@
+#!/data/data/com.termux/files/usr/bin/bash
+COMMENT="🔥 Sprawdź ten produkt: https://example.com/produkt 🔥"
+termux-notification --title "Efekt Kruka" --content "$COMMENT" --priority high
